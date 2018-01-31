76c14b9ab76e9fc8f8571693c143e0c28bad8ced
cs8433 <christian.hoerauf@consorsbank.de>
2018-02-01 00:23:29 +0100

    make it bluemix complient

--- bin/server.js
+++ bin/server.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+const cfenv = require('cfenv')
+const appEnv = cfenv.getAppEnv()
 const logger = require('../lib/logger')(1000, console)
 const serverModule = require('../lib/server')(logger)
-serverModule.init(3000, () => {})
+serverModule.init(appEnv.port, () => {logger.log(`server startin on ${appEnv.url}`)})
