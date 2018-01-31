800d3f1889b892e1ff874203146032227a8fdc69
cs8433 <christian.hoerauf@consorsbank.de>
2018-01-31 22:36:24 +0100

    logging implemented

--- bin/server.js
+++ bin/server.js
@@ -1,2 +1,4 @@
-const serverModule = require('../lib/server')(console)
+#!/usr/bin/env node
+const logger = require('../lib/logger')(1000, console)
+const serverModule = require('../lib/server')(logger)
 serverModule.init(3000, () => {})
