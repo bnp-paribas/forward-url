f613b957b6e99dd733be342dbcc234a2c31c211d
cs8433 <christian.hoerauf@consorsbank.de>
2018-02-08 23:08:59 +0100

    Ignore self signed certificates

--- bin/server.js
+++ bin/server.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
+
+// ignore self signed certificates
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 const cfenv = require('cfenv')
 const appEnv = cfenv.getAppEnv()
 const logger = require('../lib/logger')(1000, console)
